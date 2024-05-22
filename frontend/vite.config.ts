ed4ce05c5fbd96ecff96782b03fc3e21b83fb389
김동훈 <huni19541@gmail.com>
2024-05-22 13:12:54 +0900

    [Fix] 수정 FE vite.config.ts
    -
    
    [Resolves] main

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,62 +1,38 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), svgr()],
-  server: {
-    port: 3333,
-  },
-  resolve: {
-    alias: [
-      {
-        find: '@',
-        replacement: '/src',
-      },
-      {
-        find: '@api',
-        replacement: '/src/api',
-      },
-      {
-        find: '@assets',
-        replacement: '/src/assets',
-      },
-      {
-        find: '@components',
-        replacement: '/src/components',
-      },
-      {
-        find: '@global',
-        replacement: '/src/global',
-      },
-      {
-        find: '@hooks',
-        replacement: '/src/hooks',
-      },
-      {
-        find: '@pages',
-        replacement: '/src/pages',
-      },
-      {
-        find: '@routes',
-        replacement: '/src/routes',
-      },
-      {
-        find: '@types',
-        replacement: '/src/types',
-      },
-    ],
-  },
-  build: {
-    rollupOptions: {
-      onwarn: (warning, warn) => {
-        // Suppress "Module level directives cause errors when bundled" warnings
-        if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
-          return;
-        }
-        warn(warning);
+export default defineConfig(({ mode }) => {
+  // 모드에 따라 환경 변수 로드
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react(), svgr()],
+    base: env.VITE_BASE_URL ? env.VITE_BASE_URL : '/', // 환경 변수 사용
+    server: {
+      port: 3333,
+    },
+    resolve: {
+      alias: [
+        { find: '@', replacement: '/src' },
+        { find: '@api', replacement: '/src/api' },
+        { find: '@assets', replacement: '/src/assets' },
+        { find: '@components', replacement: '/src/components' },
+        { find: '@global', replacement: '/src/global' },
+        { find: '@hooks', replacement: '/src/hooks' },
+        { find: '@pages', replacement: '/src/pages' },
+        { find: '@routes', replacement: '/src/routes' },
+      ],
+    },
+    build: {
+      rollupOptions: {
+        onwarn: (warning, warn) => {
+          if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
+            return;
+          }
+          warn(warning);
+        },
       },
     },
-  },
+  };
 });
