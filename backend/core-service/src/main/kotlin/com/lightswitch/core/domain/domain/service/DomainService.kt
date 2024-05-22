cf3f8231c7d267581a133baff309f3890b950c54
김동훈 <huni19541@gmail.com>
2024-05-22 13:30:36 +0900

    [Fix] 추가 BE CORS
    -
    
    [Resolves] main

---
+++ backend/core-service/src/main/kotlin/com/lightswitch/core/domain/domain/service/DomainService.kt
@@ -0,0 +1,45 @@
+package com.lightswitch.core.domain.domain.service
+
+import com.lightswitch.core.common.config.CorsConfig
+import com.lightswitch.core.domain.domain.repository.DomainRepository
+import com.lightswitch.core.domain.domain.repository.entity.Domain
+import jakarta.annotation.PostConstruct
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+
+@Service
+class DomainService {
+
+    @Autowired
+    lateinit var domainRepository: DomainRepository
+
+    @Autowired
+    lateinit var corsConfig: CorsConfig
+
+    @Value("\${spring.cors.domain}")
+    lateinit var domain: String
+
+    fun addCors(domains: List<String>) {
+        val existsDomains = domainRepository.findAll()
+
+        domains.map {
+            if (existsDomains.none { existDomain -> existDomain.domain == it }) {
+                domainRepository.save(Domain(domain = it))
+            }
+        }
+
+        corsConfig.updateCorsConfiguration(domains)
+    }
+
+    @PostConstruct
+    fun initCors() {
+        var domains = domainRepository.findAll().map { it.domain }
+        if (domains.isEmpty()) {
+            domains = listOf(domain)
+            domainRepository.saveAll(domains.map { Domain(domain = it) })
+        }
+        corsConfig.updateCorsConfiguration(domains)
+    }
+
+}
